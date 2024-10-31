82999dcb03ea53492f0426de224482b470e590f2
NomzamoM11 <mnomzamo83@gmail.com>
2024-10-31 13:34:33 +0200

    Updated Axios link and added search functionality  using an API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = (response.data.temperature.current)
+  let cityElement = document.querySelector("#city");
+  
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "4507d3fa7d70aoadbteec32b558d1f39";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Paris");
